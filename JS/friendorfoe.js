a485b7d2f45ec24c80deca4aff94abe0040e4648
Michael Bender <michael.bender@spartronics.com>
2023-05-01 11:27:04 -0400

    never did this going back to react

---
+++ JS/friendorfoe.js
@@ -0,0 +1,10 @@
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+// z   xIf a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+function friend(friends) {
+    return 'blah';   
+}
+
+console.log(friend(Input));
