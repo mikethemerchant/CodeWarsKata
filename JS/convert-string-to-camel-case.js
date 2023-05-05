13ef5da853ee388d843557002bcd74ff48c01daa
Michael Bender <michael.bender@spartronics.com>
2023-05-05 09:22:45 -0400

    his version combined the functions

--- JS/convert-string-to-camel-case.js
+++ JS/convert-string-to-camel-case.js
@@ -17,4 +17,36 @@ function toCamelCase(str){
     return ary.join('');
 }
 
-console.log(toCamelCase(str));
+
+
+// one liner
+function toCamelCase2(str){
+    return str.split(/-|_/g) // split string into array 
+               // Q: does this split on the - and _?
+                // A: yes, the g flag means global, so it will split on all instances of - and _
+              .map((w, i) => // loop through array
+                (i > 0 ? w.charAt(0).toUpperCase() : w.charAt(0)) // uppercase if not first letter // or it just returns the char unchanged
+                // Q: what does charAt(0) do?
+                // A: returns the first character of a string // then it uppercases it
+
+              + w.slice(1))
+                // q: what does the + do?
+                // a: it concatenates the strings
+                // Q: what does slice(1) do?
+                // A: returns the rest of the string after the first character
+              
+              .join('');
+              // this joins the sub strings back together
+  }
+
+  console.log(toCamelCase2(str));
+
+  // ok lets see what you create
+
+//create a function that will take a string, remove the - and _ and return the first letter of each word capitalized, but not the first letter of the string
+// example: the-stealth-warrior
+// output: TheStealthWarrior
+
+function convertString (str) {
+    return str.split(/-|_/g).map((w, i) => (i > 0 ? w.charAt(0).toUpperCase() : w.charAt(0)) + w.slice(1)).join('');
+}
