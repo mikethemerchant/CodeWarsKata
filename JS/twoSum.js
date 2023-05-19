be458ce98eb26032eea51acbe90a2d6cf41698d6
mikethemerchant <mikethemerchant@gmail.com>
2023-05-19 12:59:47 -0400

    his

--- JS/twoSum.js
+++ JS/twoSum.js
@@ -1,11 +1,12 @@
-
+// Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+//
 function twoSum(numbers, target) {
-    for(let i=0;i<numbers.length;i++) {
-        for(let z=0;z<numbers.length;z++) {
-            if(i != z) {
-                if(numbers[i] + numbers[z] === target) {
-                    return [i,z];
-                }
+    for (let i = 0; i < numbers.length; i++) {
+        const num1 = numbers[i];
+        for (let j = i + 1; j < numbers.length; j++) {
+            const num2 = numbers[j];
+            if (num1 + num2 === target) {
+                return [i, j];
             }
         }
     }
